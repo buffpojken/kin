48a65d84c710e34890b86f4c033e2c5467cf350a
Thomas Mertz <tm@peytz.dk>
2014-11-27 15:58:42 +0100

    Cleaned up the timeline

--- includes/templates/wall.inc.php
+++ includes/templates/wall.inc.php
@@ -5,17 +5,3 @@
 	</div>
 	<button type="submit" class="btn btn-default">Post</button>
 </form>
-
-<ul class="nav nav-tabs" role="tablist">
-	<li role="presentation" class="active"><a href="#everyone" aria-controls="home" role="tab" data-toggle="tab">Everyone</a></li>
-	<li role="presentation"><a href="#friends" aria-controls="profile" role="tab" data-toggle="tab">Friends</a></li>
-</ul>
-
-<div class="tab-content">
-	<div role="tabpanel" class="tab-pane active" id="everyone">
-		<p>Everyone</p>
-	</div>
-	<div role="tabpanel" class="tab-pane" id="friends">
-		<p>Friends</p>
-	</div>
-</div>
