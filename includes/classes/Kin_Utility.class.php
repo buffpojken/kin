791a0e26710fb03a25e3b89772c63f4006d6a958
Thomas Mertz <tm@peytz.dk>
2014-11-28 15:26:12 +0100

    Renamed updateTimeSince to timeSince, to make it more generically available. Updated the function to be even more accurate, and finally updated wall.inc.php so it references the correct function.

--- includes/classes/Kin_Utility.class.php
+++ includes/classes/Kin_Utility.class.php
@@ -1,27 +1,51 @@
 <?php
 class Kin_Utility {
 	
-	public function updateTimeSince($timestamp) {
+	public function timeSince($timestamp) {
 		$date = new DateTime();
 		$date->setTimestamp(strtotime($timestamp));
 		$interval = $date->diff(new DateTime('now'));
 		#echo '<pre>' . print_r( $interval, true ) . '</pre>';
 		if( $interval->y != 0 ) {
-			$format .= '%y years, ';
+			if( $interval->y == 1 ) {
+				$format .= '%y year, ';
+			} else {
+				$format .= '%y years, ';
+			}
 		}
 		if( $interval->m != 0 ) {
-			$format .= '%m months, ';
+			if( $interval->m == 1 ) {
+				$format .= '%m month, ';
+			} else {
+				$format .= '%m months, ';
+			}
 		}
 		if( $interval->d != 0 ) {
-			$format .= '%d days, ';
+			if( $interval->d == 1 ) {
+				$format .= '%d day, ';
+			} else {
+				$format .= '%d days, ';
+			}
 		}
 		if( $interval->h != 0 ) {
-			$format .= '%h hrs ';
+			if( $interval->h == 1 ) {
+				$format .= '%h hr ';
+			} else {
+				$format .= '%h hrs ';
+			}
 		}
 		if( $interval->h != 0 && $interval->i != 0 ) {
-			$format .= 'and %i mins ago';
+			if( $interval->i == 1 ) {
+				$format .= 'and %i min ago';
+			} else {
+				$format .= 'and %i mins ago';
+			}
 		} elseif( $interval->i != 0 ) {
-			$format .= '%i mins ago';
+			if( $interval->i == 1 ) {
+				$format .= '%i min ago';
+			} else {
+				$format .= '%i mins ago';
+			}
 		}
 		echo $interval->format( $format );
 	}
