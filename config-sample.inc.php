649a9b04def2388809ee3ef9b6793ab54d5dec32
Thomas Mertz <tm@peytz.dk>
2014-11-30 18:48:50 +0100

    Updated config-sample.inc.php

--- config-sample.inc.php
+++ config-sample.inc.php
@@ -2,13 +2,15 @@
 //==================================
 //! SITE SETTINGS
 //==================================
-define('ENCRYPTION_SALT', '[[INSTALLER_REPLACE_ENCRYPTION_SALT]]');
-define('DB_TABLE_PREFIX', '[[INSTALLER_REPLACE_DATABASE_PREFIX]]');
+define('ENCRYPTION_SALT', '');
+define('DB_TABLE_PREFIX', '');
+define('KIN_VERSION', '1.0');
+date_default_timezone_set('Europe/Copenhagen');
 
 //==================================
 //! ERROR HANDLING AND DEBUG
 //==================================
-error_reporting('E_ALL');
+error_reporting('E_NONE');
 
 //==================================
 //! PATHS
@@ -17,28 +19,27 @@ define('INCLUDE_PATH',realpath(dirname(__FILE__)).'/includes');
 define('CLASSES_PATH',realpath(dirname(__FILE__)).'/includes/classes');
 define('LIBRARY_PATH',realpath(dirname(__FILE__)).'/includes/libraries');
 define('TEMPLATE_PATH',realpath(dirname(__FILE__)).'/includes/templates');
+define('GLOBALS_PATH',realpath(dirname(__FILE__)).'/includes/globals');
+define('UPLOADS_PATH',realpath(dirname(__FILE__)).'/uploads');
 
 //==================================
 //! DATABASE CONNECTION
 //==================================
 require_once(LIBRARY_PATH."/ezsql/ez_sql_core.php");
 require_once(LIBRARY_PATH."/ezsql/ez_sql_mysqli.php");
-$dbUser = "[[INSTALLER_REPLACE_DATABASE_USERNAME]]";
-$dbPass = "[[INSTALLER_REPLACE_DATABASE_PASSWORD]]";
-$dbName = "[[INSTALLER_REPLACE_DATABASE_NAME]]";
-$dbHost = "[[INSTALLER_REPLACE_DATABASE_HOST]]";
+$dbUser = "root";
+$dbPass = "Bash49350653";
+$dbName = "tmertz_dev.kin.com";
+$dbHost = "localhost";
 $db = new ezSQL_mysqli($dbUser,$dbPass,$dbName,$dbHost);
 
 //==================================
 //! CLASS LOADER
 //==================================
-require(CLASSES_PATH . '/loader.inc.php');
-
-//==================================
-//! SITE SETTINGS
-//==================================
-$locale = getSysOption('language');
-putenv("LC_ALL=$locale");
-setlocale(LC_ALL, $locale);
-bindtextdomain("messages", "./Locale");
-textdomain("messages");
+//require(CLASSES_PATH . '/loader.inc.php');
+require(CLASSES_PATH . '/Kin_User.class.php');
+require(CLASSES_PATH . '/Kin_Updates.class.php');
+require(CLASSES_PATH . '/Kin_Notification.class.php');
+require(CLASSES_PATH . '/Kin_Utility.class.php');
+$utility = new Kin_Utility;
+$notifications = new Kin_Notification;
