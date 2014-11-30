5d31972423fc3d3e6bad32d2b098c10330a7a71c
Thomas Mertz <tm@peytz.dk>
2014-11-30 20:39:48 +0100

    Add a small tweak to the login procedure.

--- includes/classes/Kin_User.class.php
+++ includes/classes/Kin_User.class.php
@@ -31,8 +31,7 @@ class Kin_User {
 		$email = $db->escape($email);
 		$hashedPassword = sha1( $password . ENCRYPTION_SALT );
 		
-		$userID = $db->get_var( "SELECT id FROM ".DB_TABLE_PREFIX."users WHERE email = '{$email}' AND passwordResetHash = '0'" );
-		$db->debug();
+		$userID = $db->get_var( "SELECT id FROM ".DB_TABLE_PREFIX."users WHERE email = '{$email}'" );
 		if( !is_numeric( $userID ) ) {
 			$errors['db_info'] = "We were unable to find any user with that combination of email and password. Please try again.";
 		}
