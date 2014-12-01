4681ae2b571ff502df215fbad8399738ffbb0081
Thomas Mertz <tm@peytz.dk>
2014-12-01 23:50:03 +0100

    Fixed a typo in the header

--- includes/globals/header.inc.php
+++ includes/globals/header.inc.php
@@ -4,7 +4,7 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<!-- WEBAPP STUFF -->
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scaleable=no">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">.
 		<meta name="mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black" />
