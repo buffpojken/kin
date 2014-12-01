492c33fb9482615b63b20ecd5d6ad7bf63e00fca
Thomas Mertz <tm@peytz.dk>
2014-12-01 17:05:16 +0100

    Updated friends list.

--- includes/templates/friends.inc.php
+++ includes/templates/friends.inc.php
@@ -6,9 +6,15 @@
 	if( $friends = $user->returnFriendsUserIDs( $_SESSION['userID'] ) ) {
 	foreach( $friends as $friend ) {
 		$profile = new Kin_User($friend);
-		$output .= '<li class="user col-sm-4"><a href="/profile/'.$profile->username.'">' . PHP_EOL;
-		$output .= '<img src="/uploads/avatars/'.$profile->userID.'-150x150.jpg" class="portrait" />' . PHP_EOL;
-		$output .= '<h4>'.$profile->name . ' ' . $profile->surname . '</h4>' . PHP_EOL;
+		$firstInitial = substr($profile->name, 0, 1);
+		$lastInitial = substr($profile->surname, 0, 1);
+		$output .= '<li class="user col-sm-6"><a href="/profile/'.$profile->username.'">' . PHP_EOL;
+		if( file_exists( UPLOADS_PATH . '/avatars/'.$profile->userID.'-40x40.jpg' ) ) {
+			$output .= '<img src="/uploads/avatars/'.$profile->userID.'-40x40.jpg" class="portrait" />' . PHP_EOL;
+		} else {
+			$output .= '<img src="http://placehold.it/40/158cba/ffffff&text='.$firstInitial.'+'.$lastInitial.'" class="portrait" />' . PHP_EOL;
+		}
+		$output .= '<h5>'.$profile->name . ' ' . $profile->surname . '</h5>' . PHP_EOL;
 		$output .= '</a></li>' . PHP_EOL;
 		unset($profile);
 	}
