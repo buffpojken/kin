c5c26059077109850cfce0aa1e68e497afc475d4
Thomas Mertz <tm@peytz.dk>
2014-12-01 17:09:50 +0100

    And now with glorious fallback portraits.

--- includes/globals/sidebar.inc.php
+++ includes/globals/sidebar.inc.php
@@ -1,7 +1,14 @@
 <div class="col-md-3" id="sidebar">
 	<?php if( isset( $_SESSION["userID"] ) ) { ?>
 	<aside class="widget" id="profile">
-		<img src="/uploads/avatars/<?php echo $_SESSION['userID']; ?>-150x150.jpg" class="portrait" />
+		<?php
+		$firstInitial = substr($user->getUserData($_SESSION["userID"],'name', FALSE), 0, 1);
+		$lastInitial = substr($user->getUserData($_SESSION["userID"],'surname', FALSE), 0, 1);
+		if( file_exists( UPLOADS_PATH . '/avatars/'.$profile->userID.'-150x150.jpg' ) ) {
+			echo '<img src="/uploads/avatars/'.$profile->userID.'-150x150.jpg" class="portrait" />' . PHP_EOL;
+		} else {
+			echo '<img src="http://placehold.it/150/158cba/ffffff&text='.$firstInitial.'+'.$lastInitial.'" class="portrait" />' . PHP_EOL;
+		} ?>
 		<h4>Hi, <?php $user->getUserData($_SESSION["userID"],'name', TRUE); ?> <?php $user->getUserData($_SESSION["userID"],'surname', TRUE); ?></h4>
 		<p><a href="/profile">Profile</a> · <a href="/friends">Friends</a> · <a href="/logout">Log out</a></p>
 	</aside>
