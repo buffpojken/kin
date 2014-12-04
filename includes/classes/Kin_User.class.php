36219d02a3aed18d3ad58ab69d19464c07367655
Thomas Mertz <tm@peytz.dk>
2014-12-04 11:39:03 +0100

    Finished the password reset functionality. Everything seems to be in order.

--- includes/classes/Kin_User.class.php
+++ includes/classes/Kin_User.class.php
@@ -9,22 +9,24 @@ class Kin_User {
 	var $siteAdmin;
 	
 	function __construct($userIdentificer=FALSE) {
-		global $db;
-		$userIdentifier = $db->escape($userIdentifier);
-		if( is_numeric( $userIdentificer ) ) {
-			$data = $db->get_row("SELECT * FROM ".DB_TABLE_PREFIX."users WHERE id = '{$userIdentificer}'");
-		} elseif( filter_var( $userIdentifier, FILTER_VALIDATE_EMAIL ) ) {
-			$data = $db->get_row("SELECT * FROM ".DB_TABLE_PREFIX."users WHERE username = '{$userIdentificer}'");
-		} else {
-			$data = $db->get_row("SELECT * FROM ".DB_TABLE_PREFIX."users WHERE username = '{$userIdentificer}'");
+		if( $userIdentificer ) {
+			global $db;
+			$userIdentifier = $db->escape($userIdentifier);
+			if( is_numeric( $userIdentificer ) ) {
+				$data = $db->get_row("SELECT * FROM ".DB_TABLE_PREFIX."users WHERE id = '{$userIdentificer}'");
+			} elseif( filter_var( $userIdentifier, FILTER_VALIDATE_EMAIL ) ) {
+				$data = $db->get_row("SELECT * FROM ".DB_TABLE_PREFIX."users WHERE username = '{$userIdentificer}'");
+			} else {
+				$data = $db->get_row("SELECT * FROM ".DB_TABLE_PREFIX."users WHERE username = '{$userIdentificer}'");
+			}
+			$this->userID = $data->id;
+			$this->name = $data->name;
+			$this->surname = $data->surname;
+			$this->username = $data->username;
+			$this->email = $data->email;
+			$this->siteAdmin = $data->siteAdmin;
+			return $this;
 		}
-		$this->userID = $data->id;
-		$this->name = $data->name;
-		$this->surname = $data->surname;
-		$this->username = $data->username;
-		$this->email = $data->email;
-		$this->siteAdmin = $data->siteAdmin;
-		return $this;
 	}
 	
 	public function authorize($email, $password, $cookie) {
@@ -46,6 +48,9 @@ class Kin_User {
 		
 		if( $this->getUserData( $userID, 'password' ) == $hashedPassword ) {
 			$_SESSION['userID'] = $userID;
+			if( $cookie == 'yes' ) {
+				setcookie ( 'kin_social_login', $this->getUserData($userID,'userHash'), time() + 60 * 60 * 24 * 14 );
+			}
 			HEADER('Location: /');
 		} else {
 			$errors['db_info'] = "We were unable to find any user with that combination of email and password. Please try again.";
@@ -57,9 +62,6 @@ class Kin_User {
 				echo '<li>'.$error.'</li>';
 			}
 			echo '</ul></div>';
-		}
-		if( $cookie == 'yes' ) {
-			setcookie ( 'kin_social_login', $this->getUserData($userID,'userHash'), time() + 60 * 60 * 24 * 14 );
 		}
 	}
 	
@@ -117,21 +119,33 @@ class Kin_User {
 		$mail->SMTPAuth = true;
 		$mail->Username = SMTP_USER;
 		$mail->Password = SMTP_PASS;
-		$mail->SMTPSecure = 'SSL';
+		$mail->SMTPSecure = 'ssl';
 		$mail->Port = SMTP_PORT;
 		$mail->From = 'no-reply@czochabook.me';
 		$mail->FromName = 'CzochaBook';
 		$mail->addAddress($email);
 		$mail->isHTML(true);
 		$mail->Subject = '[CzochaBook] Password Reset';
-		$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-		if(!$mail->send()) {
-			echo 'Message could not be sent.';
-			echo 'Mailer Error: ' . $mail->ErrorInfo;
+		$mail->Body    = nl2br($emailContent);
+		return ($mail->send()) ? TRUE : FALSE;
+	}
+	
+	public function resetPassword( $resetKey, $resetEmail, $resetPasswordOne, $resetPasswordTwo ) {
+		global $db;
+		if( $resetPasswordOne == $resetPasswordTwo ) {
+			$resetKey = $db->escape($resetKey);
+			$resetEmail = $db->escape($resetEmail);
+			$hashedPassword = $db->escape(sha1( $resetPasswordOne . ENCRYPTION_SALT ));
+			if( $db->query( "UPDATE ".DB_TABLE_PREFIX."users SET password='{$hashedPassword}', passwordResetHash='0' WHERE email = '{$resetEmail}' AND passwordResetHash='{$resetKey}'" ) ) {
+				return TRUE;
+			} else {
+				return FALSE;
+			}
+			
 		} else {
-			echo 'Message has been sent';
+			return FALSE;
 		}
-		#return ($mail->send()) ? TRUE : FALSE;
+		$db->debug();
 	}
 	
 	public function updateProfile( $data, $portrait ) {
--- includes/templates/login.inc.php
+++ includes/templates/login.inc.php
@@ -1,56 +1,92 @@
-<form role="form" method="post" action="">
-	<legend>Come on in ...</legend>
-	
-	<?php 
-	if( isset( $_POST['action'] ) && $_POST['action']=='executeLogin' ) {
-		$user = new Kin_User();
-		$user->authorize( $_POST['login_email'], $_POST['login_password'], $_POST['login_cookie'] );
+<?php if( isset( $_GET['reset-key'] ) && strlen( $_GET['reset-key'] ) == 40 ) { ?>
+	<form role="form" method="post" action="">
+		<legend>Password Reset</legend>
 		
-	}
-	if( isset( $_POST['action'] ) && $_POST['action']=='startPasswordReset' ) {
-		$user = new Kin_User;
-		if( $user->startPasswordReset( $_POST['reset_email'] ) ) {
-			echo '<div class="alert alert-success" role="alert"><strong>Awesome!</strong> Check your email for instructions on how to reset your password!</div>';
-		} else {
-			echo '<div class="alert alert-danger" role="alert"><strong>Whoops!</strong> We were unable to reset your password. Please try again.<br />If the problem persists, please contact a site administrator.</div>';
+		<?php 
+		if( isset( $_POST['action'] ) && $_POST['action']=='finishPasswordReset' ) {
+			$user = new Kin_User();
+			if( $user->resetPassword( $_POST['reset_key'], $_POST['reset_email'], $_POST['reset_password'], $_POST['reset_repeat_password'] ) ) {
+				echo '<div class="alert alert-success" role="alert"><strong>Awesome!</strong> Your password was successfully reset. No go <a href="/" class="alert-link">log in</a>!</div>';
+			} else {
+				echo '<div class="alert alert-danger" role="alert"><strong>Whoops!</strong> We were unable to reset your password. Please try again.<br />If the problem persists, please contact a site administrator.</div>';
+			}
 		}
-	} ?>
-	<div class="form-group">
-		<label for="login_email">Email address</label>
-		<input type="email" class="form-control" id="login_email" name="login_email" placeholder="Enter email">
-	</div>
-	<div class="form-group">
-		<label for="login_password">Password</label>
-		<input type="password" class="form-control" id="login_password" name="login_password" placeholder="Password">
-	</div>
-	<div class="checkbox">
-		<label for="login_cookie">
-			<input type="checkbox" name="login_cookie" id="login_cookie" value="yes" /> Remember me
-		</label>
-	</div>
-	<button type="submit" class="btn btn-default">Log in</button>
-	<button type="button" class="btn btn-link" data-toggle="modal" data-target="#passwordResetModal">I forgot my password!</button>
-	<input type="hidden" name="action" value="executeLogin" />
-</form>
-
-<div class="modal fade" id="passwordResetModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-	<form class="modal-dialog" method="post" action="">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-				<h4 class="modal-title" id="myModalLabel">Password Reset</h4>
-			</div>
-			<div class="modal-body">
-				<div class="form-group">
-					<label for="reset_email">Please enter your email address</label>
-					<input type="email" class="form-control" name="reset_email" id="reset_email" placeholder="Enter email" />
+		?>
+		<div class="form-group">
+			<label for="reset_key">Reset key</label>
+			<input type="text" class="form-control" id="reset_key" name="reset_key" value="<?php echo $_GET['reset-key']; ?>" disabled="disabled" />
+		</div>
+		<div class="form-group">
+			<label for="reset_email">Email address</label>
+			<input type="email" class="form-control" id="reset_email" name="reset_email" placeholder="Enter email">
+		</div>
+		<div class="form-group">
+			<label for="reset_password">Password</label>
+			<input type="password" class="form-control" id="reset_password" name="reset_password" placeholder="Password">
+		</div>
+		<div class="form-group">
+			<label for="reset_repeat_password">Repeat password</label>
+			<input type="password" class="form-control" id="reset_repeat_password" name="reset_repeat_password" placeholder="Password">
+		</div>
+		<button type="submit" class="btn btn-default">Reset password</button>
+		<input type="hidden" name="action" value="finishPasswordReset" />
+		<input type="hidden" name="reset_key" value="<?php echo $_GET['reset-key']; ?>" />
+	</form>
+<?php } else { ?>
+	<form role="form" method="post" action="">
+		<legend>Come on in ...</legend>
+		
+		<?php 
+		if( isset( $_POST['action'] ) && $_POST['action']=='executeLogin' ) {
+			$user = new Kin_User();
+			$user->authorize( $_POST['login_email'], $_POST['login_password'], $_POST['login_cookie'] );
+			
+		}
+		if( isset( $_POST['action'] ) && $_POST['action']=='startPasswordReset' ) {
+			$user = new Kin_User();
+			if( $user->startPasswordReset( $_POST['reset_email'] ) ) {
+				echo '<div class="alert alert-success" role="alert"><strong>Awesome!</strong> Check your email for instructions on how to reset your password!</div>';
+			} else {
+				echo '<div class="alert alert-danger" role="alert"><strong>Whoops!</strong> We were unable to reset your password. Please try again.<br />If the problem persists, please contact a site administrator.</div>';
+			}
+		} ?>
+		<div class="form-group">
+			<label for="login_email">Email address</label>
+			<input type="email" class="form-control" id="login_email" name="login_email" placeholder="Enter email">
+		</div>
+		<div class="form-group">
+			<label for="login_password">Password</label>
+			<input type="password" class="form-control" id="login_password" name="login_password" placeholder="Password">
+		</div>
+		<div class="checkbox">
+			<label for="login_cookie">
+				<input type="checkbox" name="login_cookie" id="login_cookie" value="yes" /> Remember me
+			</label>
+		</div>
+		<button type="submit" class="btn btn-default">Log in</button>
+		<button type="button" class="btn btn-link" data-toggle="modal" data-target="#passwordResetModal">I forgot my password!</button>
+		<input type="hidden" name="action" value="executeLogin" />
+	</form>
+	
+	<div class="modal fade" id="passwordResetModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<form class="modal-dialog" method="post" action="">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+					<h4 class="modal-title" id="myModalLabel">Password Reset</h4>
+				</div>
+				<div class="modal-body">
+					<div class="form-group">
+						<label for="reset_email">Please enter your email address</label>
+						<input type="email" class="form-control" name="reset_email" id="reset_email" placeholder="Enter email" />
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="reset" class="btn btn-default" data-dismiss="modal">Cancel</button>
+					<button type="submit" class="btn btn-primary">Reset my password</button>
+					<input type="hidden" name="action" value="startPasswordReset" />
 				</div>
 			</div>
-			<div class="modal-footer">
-				<button type="reset" class="btn btn-default" data-dismiss="modal">Cancel</button>
-				<button type="submit" class="btn btn-primary">Reset my password</button>
-				<input type="hidden" name="action" value="startPasswordReset" />
-			</div>
-		</div>
-	</form>
-</div>
+		</form>
+	</div>
+<?php } ?>
